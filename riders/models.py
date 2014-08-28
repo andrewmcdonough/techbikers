88fcea0ac2a3115d3998b7827f82f6f913ca423f
Michael Willmott <michael@knodium.com>
2014-08-28 10:02:58 +0100

    WIP: Update rider profile model

--- riders/models.py
+++ riders/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from markupfield.fields import MarkupField
 from django.contrib.auth.models import User
+
+
+def content_file_name(instance, filename):
+        return '/'.join(['content', instance.user.username, filename])
+
 
 class RiderProfile(models.Model):
     # Link to main user record
@@ -10,7 +16,11 @@ class RiderProfile(models.Model):
     website = models.URLField(null=True, blank=True)
     twitter = models.CharField(max_length=200, null=True, blank=True)
 
-    biography = models.TextField(blank=True)
+    biography = MarkupField(null=True, blank=True, markup_type='markdown')
+    statement = MarkupField(null=True, blank=True, markup_type='markdown')
+
+    avatar = models.ImageField(upload_to=content_file_name)
+    banner = models.ImageField(upload_to=content_file_name)
 
     def __unicode__(self):
         return self.user.username
